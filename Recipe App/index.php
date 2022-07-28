e7adef70f344ded3dde42e20b18fbe219626d698
James Davenport <jamesdizzle@gmail.com>
2022-07-28 15:40:49 -0700

    layout fine-tuning

--- Recipe App/index.php
+++ Recipe App/index.php
@@ -11,7 +11,7 @@
 
 <body>
     <?php
-    require_once('inc/dbconnect.inc.php');
+    // require_once('inc/dbconnect.inc.php');
 
     // $stmt = $pdo->query('SELECT * FROM author');
     // while ($row = $stmt->fetch()) {
@@ -20,8 +20,7 @@
     ?>
 
     <div class="wrapper">
-        <h1 class="grid-col-span-4">My First Recipe</h1>
-        <div class="info">
+        <h1 class="">My First Recipe</h1>
             <div id="rating">
                 <span>Rating:&nbsp;</span>
                 <i class="gold-star">
@@ -32,47 +31,51 @@
                     <img src="img/blankstar.png" alt="">
                 </i>
             </div>
-        </div>
-        <div id="img-info">
+        <div id="img-info-container">
             <div id="meal-img" class="">
                 <img src="https://source.unsplash.com/random/700x350" alt="">
             </div>
+            <div id="gallery" class="">
+                <img src="https://source.unsplash.com/random/100x75" alt="">
+                <img src="https://source.unsplash.com/random/100x75" alt="">
+                <img src="https://source.unsplash.com/random/100x75" alt="">
+                <img src="https://source.unsplash.com/random/100x75" alt="">
+            </div>
             <div id="meal-info" class="grid-col-span-1">
-                <p>Prep:</p>
-                <p>Cook:</p>
-                <p>Total:</p>
-                <p>Servings:</p>
-                <p>Yield:</p>
+                <p>Prep: <span id="prep">10 mins</span></p>
+                <p>Cook: <span id="cook">30 mins</span></p>
+                <p>Total: <span id="total">40 mins</span></p>
+                <p>Servings: <span id="servings">4</span></p>
+                <p>Yield: <span id="yield">4 servings</span></p>
             </div>
         </div>
-        <div id="gallery" class="">
-            <img src="https://source.unsplash.com/random/100x75" alt="">
-            <img src="https://source.unsplash.com/random/100x75" alt="">
-            <img src="https://source.unsplash.com/random/100x75" alt="">
-            <img src="https://source.unsplash.com/random/100x75" alt="">
-        </div>
-        <div id="blurb" class="">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, neque facilis? Accusantium beatae quibusdam a et? Totam aliquam asperiores est, quis iure cupiditate doloribus ratione tempora sint deserunt perspiciatis nostrum saepe eos doloremque quo. Repudiandae facere ab eius cupiditate officiis voluptatum, temporibus ex aspernatur totam nostrum, sit maxime voluptas illum.</p>
-        </div>
-        <div id="ingredients" class="">
+        <div class="recipe-info">
+            <div id="blurb" class="">
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Obcaecati, neque facilis? Accusantium beatae quibusdam a et? Totam aliquam asperiores est, quis iure cupiditate doloribus ratione tempora sint deserunt perspiciatis nostrum saepe eos doloremque quo. Repudiandae facere ab eius cupiditate officiis voluptatum, temporibus ex aspernatur totam nostrum, sit maxime voluptas illum.</p>
+            </div>
             <h2>Ingredients</h2>
-            <ul>
-                <li>Lorem ipsum dolor sit.</li>
-                <li>Maiores laborum repudiandae nisi.</li>
-                <li>Aliquid itaque laudantium consequatur?</li>
-                <li>Ut, alias fugit! Nostrum?</li>
-                <li>Repudiandae porro molestias labore?</li>
-            </ul>
-        </div>
-        <div id="directions" class="">
+            <div id="ingredients" class="">
+                <input type="checkbox" name="ing1" id="ing1">
+                <label for="ing1">Some Ingredient Here</label>
+            </div>
             <h2>Directions</h2>
-        </div>
-        <div id="nut-facts">
+            <div id="directions" class="">
+                <input type="checkbox" name="step1" id="step1">
+                <label for="step1">Step 1</label>
+                <div class="paragraph">
+                    <p>Here is a step for the recipe. Complete this step, and then feel free to go onto the next step.</p>
+                </div>
+            </div>
             <h2 class="">Nutrition Facts</h2>
-        </div>
-        <div id="notes" class="">
+            <div id="nutrition">
+                <p>Here are some nutrition facts about this meal. They are extensive, and brief, all at the same time!</p>
+            </div>
             <h2 class="">Notes</h2>
+            <div id="notes" class="">
+                <p>Here are some notes about this recipe. These are private, and are stored individually, per user. Please log in to use this feature.</p>
+            </div>
         </div>
+        <footer>Some Shit Here.</footer>
     </div>
 
     <script src="js/script.js"></script>
